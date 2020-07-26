4820c45f9b3dbe70f1d5048254eacd22320f9d26
zaporozhetsAnton <zaporozhetsAnton@users.noreply.github.com>
2020-07-26 17:58:00 +0300

    Update webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const common = require('./webpack.common.config');
