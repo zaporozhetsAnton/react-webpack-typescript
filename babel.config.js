0410aa70d8b0d7388d40ea66528e8d88a9385b3d
Anton Zaporozhets <zaporozhetsanton@gmail.com>
2020-11-19 18:31:49 +0200

    babel update

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage', // don't forget to install core-js package so useBuiltIns works without any additional plugins
+        corejs: 3,
+        targets: '>0.25%',
+      },
+    ],
+    '@babel/preset-react',
+  ],
 };
